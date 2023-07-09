58adf0d7f6edb62d9c9ae68b5504d7013e470dcb
Siddhant N <71310931+WatashiwaSid@users.noreply.github.com>
2023-07-09 22:11:32 +0530

    Linear Search function.
    Linear Search function.

---
+++ Searching/LinearSearch.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void LinearSearch(int array[], int size, int key)
+{
+    int flag=0;
+    int index=0;
+
+    for(int i=0; i<size; i++)
+    {
+        if(array[i]==key)
+        {
+            index=i;
+            flag=1;
+            break;
+        }
+        else    
+            continue;
+    }
+
+    if(flag!=0)
+        printf("Element %d found at index %d", key, index);
+
+    else
+        printf("Could not find the specified element");
+
+}
+
+void main()
+{
+    int size;
+    printf("Enter the size of array : ");
+    scanf("%d", &size);
+
+    int *ptr = (int*) malloc(size*sizeof(int));
+
+    printf("Enter array elements [SIZE %d] : ", size);
+    for(int i=0; i<size; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    LinearSearch(ptr, size, 3);
+}
